5f70a9c825a68a7151daa8bfd74c6f305268dc0a
Arlen Chereshnikov <chereshnikovarlen03@gmail.com>
2026-02-26 10:28:48 +0500

    unused files deleted

--- components/schools/rating/index.ts
+++
@@ -1,8 +0,0 @@
-export { StatCard } from "./stat-card";
-export { SchoolCard, type CombinedSchool } from "./school-card";
-export { SchoolsTable } from "./schools-table";
-export { SearchAndFilters } from "./search-and-filters";
-export { Charts } from "./charts";
-export { PageHeader } from "./page-header";
-export { WelcomeBanner } from "./welcome-banner";
-export { EmptyState } from "./empty-state";
